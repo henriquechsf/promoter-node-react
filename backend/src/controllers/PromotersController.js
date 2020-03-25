405f651701031b018130c526b23433dbb44d7597
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-24 23:06:35 -0300

    inicio backend e conexao com banco postgres

---
+++ backend/src/controllers/PromotersController.js
@@ -0,0 +1,26 @@
+const connection = require('../database/connection')
+
+module.exports = {
+
+    // listar todos promotores
+    async index(req, res) {
+        const { page = 1 } = req.query
+
+        const [count] = await connection('promotor').count()
+        console.log(count)
+
+        const promoters = await connection('promotor')
+            .limit(10)
+            .offset((page - 1) * 10)
+            .select([
+                'promotor.cod',
+                'promotor.nome'
+            ])
+
+        // contagem de promotores
+        res.header('X-Total-Count', count['count(*)'])
+
+        return res.json(promoters)
+    }
+
+}
---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const PromotersController = require('./controllers/PromotersController')
+
+// instancia para usar as rotas
+const routes = express.Router()
+
+// listar os promotores
+routes.get('/promoters', PromotersController.index)
+
+module.exports = routes
